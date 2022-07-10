1885263f1601d53e71cd5d37927574bf7d124ff3
rbrunney <rbrunney@student.neumont.edu>
2022-07-10 15:05:02 -0600

    Error Handling Documentation
    Adding documentation to InternalErrorOverride Class

--- E-Commerce/Microservices/Email Service/EmailService/src/main/java/com/neumont/edu/sen300/emailservice/errorhandling/InternalExceptionOverride.java
+++ E-Commerce/Microservices/Email Service/EmailService/src/main/java/com/neumont/edu/sen300/emailservice/errorhandling/InternalExceptionOverride.java
@@ -10,6 +10,12 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class InternalExceptionOverride extends ResponseEntityExceptionHandler {
 
+    /**
+     * Exception Handler method if there is an NullPointerException within the REST Service
+     * @param npe A NullPointer Exception that was caused somewhere within the REST Service
+     * @return A ResponseEntity Including an Exception Message, HttpHeaders, and a 400 Bad Request Response
+     */
+    
     @ExceptionHandler(NullPointerException.class)
     public final ResponseEntity<ExceptionMessage> entityNotFound(NullPointerException npe) {
         ExceptionMessage em = new ExceptionMessage(npe.getMessage(), 400);
