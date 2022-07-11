2af743e61718e280f9ec727a5e43db26fd0a6f07
rbrunney <rbrunney@student.neumont.edu>
2022-07-10 22:05:50 -0600

    Added MongoDB to docker-compose.yml
    Added the information for a containerized instance of MongoDB

---
+++ E-Commerce/Microservices/Order Service/OrderService/src/main/java/com/neumont/edu/sen300/orderservice/endpoints/OrderController.java
@@ -0,0 +1,10 @@
+package com.neumont.edu.sen300.orderservice.endpoints;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    
+}
