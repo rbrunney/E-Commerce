c3736582ebb11ef53ccca95d7d1375a8ee7cb269
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2022-07-12 14:41:15 -0600

    It works

--- E-Commerce/Microservices/Account Service/Controller.cs
+++ E-Commerce/Microservices/Account Service/Controller.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
+//"docker_db1": "Server=localhost,1433;database=apidb;User ID= SA;password=abc123!!@;"
+
 namespace Controllers
 {
     [ApiController]
@@ -8,8 +10,6 @@ namespace Controllers
     public class Controller : ControllerBase
     {
         private readonly AccountDB _ACDB;
-        // private readonly AddressDB _ADDB;
-        // private readonly CreditCardDB _CDB;
         
         public Controller(ILogger<Controller> logger, AccountDB acdb)
         {            
@@ -40,20 +40,20 @@ namespace Controllers
         [HttpGet("{email}")]
         public async Task<ActionResult<Account>> GetTodoItem(string email)
         {
-            var todo = await _ACDB.Accounts.FindAsync(email);
+            var account = await _ACDB.Accounts.Where(m => m.Email  == email).ToListAsync();
             
-            if(todo == null)
+            if(account == null)
             {
                 return NotFound();
             }
 
-            return Ok(todo);
+            return Ok(account);
         }
 
         [HttpPut]
         public async Task<IResult> PutTodo(Account acc1)
         {
-            var acc2 = await _ACDB.Accounts.FindAsync(acc1.Email);
+            var acc2 = await _ACDB.Accounts.FindAsync(acc1.AccountId);
 
             if(acc2 == null)
             {
@@ -65,8 +65,18 @@ namespace Controllers
             acc2.FirstName = acc1.FirstName;
             acc2.LastName = acc1.LastName;
             acc2.Password = acc1.Password;
-            acc2.PaymentInfo = acc1.PaymentInfo;
-            acc2.ShipAddress = acc1.ShipAddress;
+
+            acc2.CardNum = acc1.CardNum;
+            acc2.ExpDate = acc1.ExpDate;
+            acc2.SecurityNum = acc1.SecurityNum;
+            acc2.NameOnCard = acc1.NameOnCard;
+
+            acc2.Street = acc1.Street;
+            acc2.Apartment = acc1.Apartment;
+            acc2.City = acc1.City;
+            acc2.State = acc1.State;
+            acc2.ZipCode = acc1.ZipCode;
+            //acc2.ShipAddress = acc1.ShipAddress;
 
             await _ACDB.SaveChangesAsync();
 
@@ -74,7 +84,7 @@ namespace Controllers
         } 
 
         [HttpDelete("{id}")]
-        public async Task<IResult> DeleteTodo(long id)
+        public async Task<IResult> DeleteTodo(int id)
         {
             if(await _ACDB.Accounts.FindAsync(id) is Account account)
             {
--- E-Commerce/Microservices/Account Service/POJOs/Account_POJO.cs
+++ E-Commerce/Microservices/Account Service/POJOs/Account_POJO.cs
@@ -1,22 +1,36 @@
-
+using state_enum;
 
 public class Account
 {
+    public int AccountId { get; set; }
     //First Name
     public string FirstName {get; set;}
-
     //Last Name
     public string? LastName {get; set;}
-
     //Email
     public string Email {get; set;}
-
     //Password
     public string Password {get; set;}
 
     //Credit Card
-    public CreditCard PaymentInfo {get; set;}
+    public long CardNum {get; set;}
+    //Expiration Date
+    public string ExpDate {get; set;}
+    //CSV Number
+    public int SecurityNum {get; set;}
+    //Name
+    public string? NameOnCard {get; set;}
 
     //Address
-    public Address ShipAddress {get; set;}
+    //Street
+    public string Street {get;set;}
+    //Street 2
+    public string? Apartment {get;set;}
+    //City
+    public string City {get;set;}
+    //State
+    public string State {get;set;}
+    //ZipCode
+    public int ZipCode {get; set;}
+
 }
