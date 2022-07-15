e857cfa2d56706f06b20890752a3a8c49aaa9e73
Dragonliver <castanley77@live.com>
2022-07-15 15:15:06 -0600

    Make items works

--- E-Commerce/Front-End/Actual-Frontend/catalog.js
+++ E-Commerce/Front-End/Actual-Frontend/catalog.js
@@ -4,11 +4,33 @@ function getItems(){
     let request = new XMLHttpRequest();
     request.open("GET", url);
     request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    request.setRequestHeader('Access-Control-Allow-Origin', '*');
     request.send();
     request.onload = () => {
-        console.log(request);
+        console.log(request.responseText);
+        var items = JSON.parse(request.responseText);
+        items.forEach(element => {
+            console.log(element);
+            addRow(element['name'], element['description'], element['unitPrice']);
+        });
     };
+}
+
+function makeItem(){
+    var url = 'http://localhost:9000/ecommerce/makeitem';
+    var item = {
+        "id": "",
+        "name": document.getElementById("Name").value,
+        "description": document.getElementById('Description').value,
+        "unitPrice": document.getElementById('UnitPrice').value
+    };
+    console.log(item);
+    let request = new XMLHttpRequest();
+    request.open("POST", url);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send(JSON.stringify(item));
+    request.onload = () => {
+        location.reload();
+    };    
 }
 
 function addRow(Name, Description, UnitPrice) {
