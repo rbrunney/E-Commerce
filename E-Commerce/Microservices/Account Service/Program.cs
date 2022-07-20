6d7598801485c0b975527f12d66502ae628c4f55
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2022-07-20 13:46:47 -0600

    Republished Cart and Account

--- E-Commerce/Microservices/Account Service/Program.cs
+++ E-Commerce/Microservices/Account Service/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Steeltoe.Discovery.Client;
-
+using Steeltoe.Common.Discovery;
+using Steeltoe.Discovery.Eureka;
+using Steeltoe.Discovery;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 //builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
