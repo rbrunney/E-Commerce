52ed931b8d234cb8202cba4a721caf0620adc401
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2022-07-20 13:42:03 -0600

    Revert "eureka code added to Catalog Service"
    This reverts commit 567c2e357316cabc6b368bf4908ab9d8e1c06c8d.

--- E-Commerce/Microservices/Catalog Service/Program.cs
+++ E-Commerce/Microservices/Catalog Service/Program.cs
@@ -1,12 +1,8 @@
 using Model;
-using Steeltoe.Discovery.Client;
-using Steeltoe.Common.Discovery;
-using Steeltoe.Discovery.Eureka;
-using Steeltoe.Discovery;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddDiscoveryClient(builder.Configuration);
+
 builder.Services.Configure<ItemDatabaseSettings>(builder.Configuration.GetSection("ItemDatabase"));
 builder.Services.AddSingleton<Ser.ItemService2>();
 
