b80fb3a76893ce35a2422b1d72d7c06bff04d54e
Dragonliver <castanley77@live.com>
2022-07-21 15:42:54 -0600

    Added cors back to catalog

--- E-Commerce/Microservices/Catalog Service/Program.cs
+++ E-Commerce/Microservices/Catalog Service/Program.cs
@@ -7,9 +7,17 @@ builder.Services.AddDiscoveryClient(builder.Configuration);
 
 builder.Services.Configure<ItemDatabaseSettings>(builder.Configuration.GetSection("ItemDatabase"));
 builder.Services.AddSingleton<Ser.ItemService2>();
-
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        });
+});
 var app = builder.Build();
 app.MapControllers();
+app.UseCors();
 
 app.Run();
 //mongodb://EC:abc123!!@@localhost:27017
